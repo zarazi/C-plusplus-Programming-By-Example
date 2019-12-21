ba26fc5b2a2597391fea0d40b5b1e2a40c753a1a
zarazi <zarazi@hotmail.com>
2019-12-21 16:19:57 +0700

    Generate random number

--- PacktCppByExample-master/2_Virtual_Die/src/main.cpp
+++ PacktCppByExample-master/2_Virtual_Die/src/main.cpp
@@ -1,7 +1,14 @@
 #include <iostream>
+#include <random>
 
 int main(int argc, char* argv[])
 {
-	std::cout << "Hello world" << std::endl;
+	std::random_device random_device;
+	std::default_random_engine random_engine(random_device());
+	std::uniform_int_distribution<int> uniform_int_distribution(1, 6);
+
+	auto random_int = uniform_int_distribution(random_engine);
+	std::cout << random_int << std::endl;
+
 	return 0;
 }
